08cd0e819b33309825ace38776cf2b74774d51be
Amin Amos <aminamosahs@gmail.com>
2019-06-13 00:01:38 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,1 +1,11 @@
-def introduction()
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+introduction
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language
